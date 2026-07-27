dde1e104d61d3769e0fc65dbce410abd7306218a
Vinicius Mariani dos Santos <119473544+Mariani5472@users.noreply.github.com>
2026-07-27 10:55:10 -0300

    feat(decision): register special off-ball evaluators

--- src/application/match/decision/offball/OffBallEvaluators.ts
+++ src/application/match/decision/offball/OffBallEvaluators.ts
@@ -2,24 +2,30 @@ import { ActionEvaluator } from "../ActionEvaluator";
 import { BlockEvaluator } from "../evaluators/BlockEvaluator";
 import { ControlEvaluator } from "../evaluators/ControlEvaluator";
 import { CoverEvaluator } from "../evaluators/CoverEvaluator";
+import { GKClaimEvaluator } from "../evaluators/GKClaimEvaluator";
 import { HeaderEvaluator } from "../evaluators/HeaderEvaluator";
 import { InterceptEvaluator } from "../evaluators/InterceptEvaluator";
 import { MarkEvaluator } from "../evaluators/MarkEvaluator";
 import { MoveEvaluator } from "../evaluators/MoveEvaluator";
+import { PositionEvaluator } from "../evaluators/PositionEvaluator";
 import { PressEvaluator } from "../evaluators/PressEvaluator";
 import { ReceiveEvaluator } from "../evaluators/ReceiveEvaluator";
 import { TackleEvaluator } from "../evaluators/TackleEvaluator";
+import { TacticalFoulEvaluator } from "../evaluators/TacticalFoulEvaluator";
 
 export function createOffBallEvaluators(): ActionEvaluator[] {
   return [
     new PressEvaluator(),
     new CoverEvaluator(),
     new TackleEvaluator(),
+    new TacticalFoulEvaluator(),
     new BlockEvaluator(),
     new InterceptEvaluator(),
+    new GKClaimEvaluator(),
     new ReceiveEvaluator(),
     new HeaderEvaluator(),
     new ControlEvaluator(),
+    new PositionEvaluator(),
     new MoveEvaluator(),
     new MarkEvaluator(),
   ];
