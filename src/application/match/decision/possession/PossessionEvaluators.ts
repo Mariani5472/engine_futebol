92dc476d1258f31492c324c572d21cc3d0d7eddb
Vinicius Mariani dos Santos <119473544+Mariani5472@users.noreply.github.com>
2026-07-27 10:35:11 -0300

    feat(decision): register skill move evaluator in possession system

--- src/application/match/decision/possession/PossessionEvaluators.ts
+++ src/application/match/decision/possession/PossessionEvaluators.ts
@@ -2,6 +2,7 @@ import { ActionEvaluator } from "../ActionEvaluator";
 import { ClearEvaluator } from "../evaluators/ClearEvaluator";
 import { CrossEvaluator } from "../evaluators/CrossEvaluator";
 import { PassEvaluator } from "../evaluators/PassEvaluator";
+import { SkillMoveEvaluator } from "../evaluators/SkillMoveEvaluator";
 import { ShotEvaluator } from "../evaluators/ShotEvaluator";
 import { DribbleEvaluator } from "../evaluators/DribbleEvaluator";
 import { HoldBallEvaluator } from "../evaluators/HoldBallEvaluator";
@@ -12,6 +13,7 @@ export function createPossessionEvaluators(): ActionEvaluator[] {
     new CrossEvaluator(),
     new ShotEvaluator(),
     new DribbleEvaluator(),
+    new SkillMoveEvaluator(),
     new HoldBallEvaluator(),
     new ClearEvaluator(),
   ];
