d188e7a1931ee02b281aea487bdf583b1360d202
Vinicius Mariani dos Santos <119473544+Mariani5472@users.noreply.github.com>
2026-07-27 10:53:24 -0300

    feat(decision): register special possession evaluators

--- src/application/match/decision/possession/PossessionEvaluators.ts
+++ src/application/match/decision/possession/PossessionEvaluators.ts
@@ -1,7 +1,10 @@
 import { ActionEvaluator } from "../ActionEvaluator";
 import { ClearEvaluator } from "../evaluators/ClearEvaluator";
 import { CrossEvaluator } from "../evaluators/CrossEvaluator";
+import { FakeEvaluator } from "../evaluators/FakeEvaluator";
+import { GKDistributeEvaluator } from "../evaluators/GKDistributeEvaluator";
 import { PassEvaluator } from "../evaluators/PassEvaluator";
+import { SetPieceEvaluator } from "../evaluators/SetPieceEvaluator";
 import { SkillMoveEvaluator } from "../evaluators/SkillMoveEvaluator";
 import { ShotEvaluator } from "../evaluators/ShotEvaluator";
 import { DribbleEvaluator } from "../evaluators/DribbleEvaluator";
@@ -14,6 +17,9 @@ export function createPossessionEvaluators(): ActionEvaluator[] {
     new ShotEvaluator(),
     new DribbleEvaluator(),
     new SkillMoveEvaluator(),
+    new FakeEvaluator(),
+    new GKDistributeEvaluator(),
+    new SetPieceEvaluator(),
     new HoldBallEvaluator(),
     new ClearEvaluator(),
   ];
